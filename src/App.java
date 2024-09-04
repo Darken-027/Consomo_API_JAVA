b90afa9cb6d5b1017636d63a6aece9493239f4e8
Darken-027 <torut1638@gmail.com>
2024-09-03 22:25:52 -0500

    metodo get

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        URL url= new URL("https://api.chucknorris.io/jokes/random")
+        try{
+        URL url = new URL("https://api.chucknorris.io/jokes/random");
+
+        HttpURLConnection connection = (HttpURLConnection)  url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+
+        if(responseCode == 200){
+            
+        }else{
+            throw new RuntimeException("Error al conectar a la API Code : " + responseCode);
+        }
+
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
